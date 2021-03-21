d794abbb596b7c3c7fee6702e2f0c55927d7347f
ati <molnar.attila28@gmail.com>
2021-03-22 00:03:36 +0100

    Browser client project was added

---
+++ HomeTicketWeb/Pages/LoginPage.razor.cs
@@ -0,0 +1,20 @@
+﻿using HomeTicketWeb.Model;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeTicketWeb.Pages
+{
+    public partial class LoginPage : ComponentBase
+    {
+        LoginInfo _loginInfo = new LoginInfo();
+        string valami;
+
+        public async Task LoginRequest()
+        {
+            valami = $"{_loginInfo.UserName} - {_loginInfo.Password}";
+        }
+    }
+}
