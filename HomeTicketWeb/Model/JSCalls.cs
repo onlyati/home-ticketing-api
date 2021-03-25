dc34a33e2dd48d683f560e2a22308131b5e71830
ati <molnar.attila28@gmail.com>
2021-03-25 22:30:55 +0100

    Moveable functions is implemented for WindowTitle

---
+++ HomeTicketWeb/Model/JSCalls.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeTicketWeb.Model
+{
+    public class JSCalls
+    {
+        private readonly IJSRuntime _js;
+
+        public JSCalls(IJSRuntime js)
+        {
+            _js = js;
+        }
+
+        public async Task<BrowserDimension> GetBrowserDimensionAsync()
+        {
+            return await _js.InvokeAsync<BrowserDimension>("getDimensions");
+        }
+
+        public async Task<BoundingClientRect> GetBounds(string element)
+        {
+            return await _js.InvokeAsync<BoundingClientRect>("GetDivInformation", element);
+        }
+    }
+
+    public class BrowserDimension
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+
+    public class BoundingClientRect
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Top { get; set; }
+        public double Right { get; set; }
+        public double Bottom { get; set; }
+        public double Left { get; set; }
+    }
+}
---
+++ HomeTicketWeb/wwwroot/jsFos/fos.js
@@ -0,0 +1,10 @@
+﻿window.getDimensions = function () {
+    return {
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+};
+
+GetDivInformation = function (element) {
+    return document.getElementById(element).getBoundingClientRect();
+};
